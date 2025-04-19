e2155fd29ffbdee6627c0375292b24eeb47fa97a
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-04-19 15:53:30 +0530

    genre page listing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { GenreListComponent } from './components/genre-list/genre-list.component';
 
 export const routes: Routes = [
+  {
+    path: 'genre',
+    component: GenreListComponent
+    // loadComponent: () => import("./components/genre-list.component").then((m) => m.GenreListComponent),
+  },
   {
     path: 'user',
     component: UserComponent
