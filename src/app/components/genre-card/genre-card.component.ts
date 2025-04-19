d5a0dfb91910873a0c1d5827477255060c7426ed
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-04-19 15:02:32 +0530

    genre-card and listing generation

---
+++ src/app/components/genre-card/genre-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './genre-card.component.html',
+  styleUrl: './genre-card.component.scss'
+})
+export class GenreCardComponent {
+
+}
---
+++ src/app/components/genre-list/genre-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './genre-list.component.html',
+  styleUrl: './genre-list.component.scss'
+})
+export class GenreListComponent {
+
+}
