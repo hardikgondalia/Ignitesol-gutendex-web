ddc521f238e2c0a91e6137f9ace32182e5cc4c10
Hardik Gondalia <hardikgondalia1990@gmail.com>
2025-04-21 10:22:31 +0530

    added blank route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./components/genre-list/genre-list.component').then((m) => m.GenreListComponent),
+  },
+  {
     path: 'genre',
     loadComponent: () => import('./components/genre-list/genre-list.component').then((m) => m.GenreListComponent),
   },
